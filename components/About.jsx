79f5fb78de2118ce376cdbc3ef1e9ffa17cac358
AsmaeBk <67161636+AsmaeBk@users.noreply.github.com>
2026-02-27 13:10:23 +0000

    correct img import

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,9 @@
+import aastroHero from "../img/aastro.png"; 
 export default function About () {
     return (
         <div className="about">
             <div className="crop-container">
-                <img src='../img/aastro.png' alt="Astro Van"/>
+                <img src={aastroHero} alt="Astro Van"/>
             </div>
             <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
             <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
